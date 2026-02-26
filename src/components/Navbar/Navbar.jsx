c590c931bd955b8b8952d775687e7d78d2eb2e02
Dhrumil436 <2301030400075@silveroakuni.ac.in>
2026-02-26 14:42:42 +0530

    Fixed SearchBar import path

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import { Link, useNavigate, useLocation } from "react-router-dom";
 import DarkMode from "./DarkMode";
 import { useCart } from "../../context/CartContext";
 import { useAuth } from "../../context/AuthContext";
-import SearchBar from "../../components/SearchBar/SearchBar";
+import SearchBar from "../SearchBar/SearchBar";
 
 const Menu = [
   { id: 1, name: "Home", link: "/" },
